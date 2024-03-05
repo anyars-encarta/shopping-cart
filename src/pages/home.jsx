aec7d62907d88839605bc04ebbbb168f761e3539
anyars-encarta <anyarsencarta@gmail.com>
2024-03-05 20:22:29 +0000

    Correct styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
                     <Circles height={'120'} width={'120'} color='rgb(127,29,29)' visible={true} />
                 </div> :
 
-                <div className='min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3'>
+                <div className='min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3'>
                     {
                         products && products.length ?
                             products.map(productItem => (
