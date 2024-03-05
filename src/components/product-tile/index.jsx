7620ab961e991482e019e61372fcc5a20c9fe108
anyars-encarta <anyarsencarta@gmail.com>
2024-03-05 18:15:36 +0000

    Fetch data from api and render on Home page

---
+++ src/components/product-tile/index.jsx
@@ -0,0 +1,18 @@
+const ProductTile = ({ product }) => {
+    // console.log('Product Tile:', product)
+    return (
+        <div>
+            <div className='group flex flex-col item-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
+                <div className='h-[180px]'>
+                    <img
+                        src={product?.image}
+                        alt={product?.title}
+                        className='object-cover h-full w-full'
+                    />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default ProductTile;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,55 @@
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
+import ProductTile from "../components/product-tile";
+
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchProducts = async () => {
+        setLoading(true);
+
+        try {
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data = await response.json();
+
+            if (data && data.length > 0) {
+                setLoading(false)
+                setError(null)
+                setProducts(data)
+            }
+        } catch (e) {
+            setError(e.message)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    if (error) {
+        return (<h3>There was an error. {error}</h3>)
+    }
+
     return (<div>
-        Home page
+        {
+            loading ?
+                <div className='min-h-screen w-full flex justify-center items-center'>
+                    <Circles height={'120'} width={'120'} color='rgb(127,29,29)' visible={true} />
+                </div> :
+
+                <div className='min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3'>
+                    {
+                        products && products.length ?
+                            products.map(productItem => (
+                                <ProductTile product={productItem} />
+                            ))
+                            : null
+                    }
+                </div>
+        }
     </div>)
 };
 
