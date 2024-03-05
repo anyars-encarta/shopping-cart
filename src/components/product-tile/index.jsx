ee311ea1a946056f2af269ae3eda8eaa528efab0
anyars-encarta <anyarsencarta@gmail.com>
2024-03-05 18:16:46 +0000

    Correct ProductTile render src

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -1,12 +1,11 @@
 const ProductTile = ({ product }) => {
-    // console.log('Product Tile:', product)
     return (
         <div>
             <div className='group flex flex-col item-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
                 <div className='h-[180px]'>
                     <img
-                        src={product?.image}
-                        alt={product?.title}
+                        src={product.image}
+                        alt={product.title}
                         className='object-cover h-full w-full'
                     />
                 </div>
