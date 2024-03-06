4fc4c2b01793718057294f2c7279b83b6ea77ae6
anyars-encarta <anyarsencarta@gmail.com>
2024-03-06 13:36:56 +0000

    Complete add to cart

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -15,7 +15,7 @@ const ProductTile = ({ product }) => {
 
     return (
         <div>
-            <div className='group flex flex-col item-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
+            <div className='group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
                 <div className='h-[180px]'>
                     <img
                         src={product?.image}
@@ -28,7 +28,7 @@ const ProductTile = ({ product }) => {
                     <h1 className='w-40 truncate mt-3 text-gray-700 font-bold text-lg'>{product?.title}</h1>
                 </div>
 
-                <div className='flex item-center justify-center w-full mt-5'>
+                <div className='flex items-center justify-center w-full mt-5'>
                     <button
                         onClick={cart.some(item => item.id === product.id) ? handleRemoveFromCart : handleAddToCart}
                         className='bg-red-950 text-white border-2 rounded-lg font-bold p-4'
