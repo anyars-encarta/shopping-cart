6a8a881e674048ad4af15b9c59c5e6ee51a500ba
anyars-encarta <anyarsencarta@gmail.com>
2024-03-06 11:38:03 +0000

    Implement remove from cart

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -1,13 +1,18 @@
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../store/slices/card-slice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../../store/slices/card-slice';
 
 const ProductTile = ({ product }) => {
     const dispatch = useDispatch();
+    const {cart} = useSelector(state=>state);
 
     const handleAddToCart = () => {
         dispatch(addToCart(product))
     };
-    
+
+    const handleRemoveFromCart = () => {
+        dispatch(removeFromCart(product.id))
+    };
+
     return (
         <div>
             <div className='group flex flex-col item-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
@@ -25,10 +30,12 @@ const ProductTile = ({ product }) => {
 
                 <div className='flex item-center justify-center w-full mt-5'>
                     <button
-                        onClick={handleAddToCart}
+                        onClick={cart.some(item => item.id === product.id) ? handleRemoveFromCart : handleAddToCart}
                         className='bg-red-950 text-white border-2 rounded-lg font-bold p-4'
                     >
-                        Add to Cart
+                        {
+                            cart.some(item => item.id === product.id) ? 'Remove from Cart' : 'Add to Cart'
+                        }
                     </button>
                 </div>
             </div>
