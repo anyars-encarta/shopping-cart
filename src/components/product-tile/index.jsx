b1ba5c5b516600518bca91a28c71ca6807d914d2
anyars-encarta <anyarsencarta@gmail.com>
2024-03-06 13:51:17 +0000

    Install prop-types

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -2,45 +2,46 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToCart, removeFromCart } from '../../store/slices/card-slice';
 
 const ProductTile = ({ product }) => {
-    const dispatch = useDispatch();
-    const {cart} = useSelector(state=>state);
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state);
 
-    const handleAddToCart = () => {
-        dispatch(addToCart(product))
-    };
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+  };
 
-    const handleRemoveFromCart = () => {
-        dispatch(removeFromCart(product.id))
-    };
+  const handleRemoveFromCart = () => {
+    dispatch(removeFromCart(product.id));
+  };
 
-    return (
-        <div>
-            <div className='group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
-                <div className='h-[180px]'>
-                    <img
-                        src={product?.image}
-                        alt={product?.title}
-                        className='object-cover h-full w-full'
-                    />
-                </div>
-
-                <div>
-                    <h1 className='w-40 truncate mt-3 text-gray-700 font-bold text-lg'>{product?.title}</h1>
-                </div>
-
-                <div className='flex items-center justify-center w-full mt-5'>
-                    <button
-                        onClick={cart.some(item => item.id === product.id) ? handleRemoveFromCart : handleAddToCart}
-                        className='bg-red-950 text-white border-2 rounded-lg font-bold p-4'
-                    >
-                        {
-                            cart.some(item => item.id === product.id) ? 'Remove from Cart' : 'Add to Cart'
-                        }
-                    </button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+        <div className="h-[180px]">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="object-cover h-full w-full"
+          />
         </div>
-    )
+
+        <div>
+          <h1 className="w-40 truncate mt-3 text-gray-700 font-bold text-lg">{product?.title}</h1>
+        </div>
+
+        <div className="flex items-center justify-center w-full mt-5">
+          <button
+            type="button"
+            onClick={cart.some((item) => item.id === product.id) ? handleRemoveFromCart : handleAddToCart}
+            className="bg-red-950 text-white border-2 rounded-lg font-bold p-4"
+          >
+            {
+                            cart.some((item) => item.id === product.id) ? 'Remove from Cart' : 'Add to Cart'
+                        }
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductTile;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,56 +1,67 @@
-import { useEffect, useState } from "react";
-import { Circles } from "react-loader-spinner";
-import ProductTile from "../components/product-tile";
+import { useEffect, useState } from 'react';
+import { Circles } from 'react-loader-spinner';
+import ProductTile from '../components/product-tile';
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const fetchProducts = async () => {
-        setLoading(true);
+  const fetchProducts = async () => {
+    setLoading(true);
 
-        try {
-            const response = await fetch('https://fakestoreapi.com/products');
-            const data = await response.json();
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
 
-            if (data && data.length > 0) {
-                setLoading(false)
-                setError(null)
-                setProducts(data)
-            }
-        } catch (e) {
-            setError(e.message)
-            setLoading(false)
-        }
+      if (data && data.length > 0) {
+        setLoading(false);
+        setError(null);
+        setProducts(data);
+      }
+    } catch (e) {
+      setError(e.message);
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    if (error) {
-        return (<h3>There was an error. {error}</h3>)
-    }
+  if (error) {
+    return (
+      <h3>
+        There was an error.
+        {error}
+      </h3>
+    );
+  }
 
-    return (<div>
-        {
-            loading ?
-                <div className='min-h-screen w-full flex justify-center items-center'>
-                    <Circles height={'120'} width={'120'} color='rgb(127,29,29)' visible={true} />
-                </div> :
+  return (
+    <div>
+      {
+            loading
+              ? (
+                <div className="min-h-screen w-full flex justify-center items-center">
+                  <Circles height="120" width="120" color="rgb(127,29,29)" visible />
+                </div>
+              )
 
-                <div className='min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3'>
-                    {
-                        products && products.length ?
-                            products.map(productItem => (
-                                <ProductTile product={productItem} />
-                            ))
-                            : null
+              : (
+                <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+                  {
+                        products && products.length
+                          ? products.map((productItem) => (
+                            <ProductTile product={productItem} />
+                          ))
+                          : null
                     }
                 </div>
+              )
         }
-    </div>)
+    </div>
+  );
 };
 
 export default Home;
