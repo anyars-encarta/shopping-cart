0a5fc737dd6f333cd0ee70adf112ad841f20bd9f
anyars-encarta <anyarsencarta@gmail.com>
2024-03-06 18:54:01 +0000

    Add fixed Navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div>
-    <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto">
+    {/* <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto"> */}
+    <nav className='flex items-center justify-between pl-28 pr-28 mx-auto fixed top-0  w-full   bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10'>
       <Link to="/">
         <div className="ml-5">
           <h1 className="text-red-900 font-bold text-xl sm:text-2xl md: text-3xl cursor-pointer tracking-wide">
