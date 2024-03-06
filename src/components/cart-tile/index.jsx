2cc0763c201de83613112ef8fb54ee4f06f9b58e
anyars-encarta <anyarsencarta@gmail.com>
2024-03-06 14:16:18 +0000

    Correct styling

--- src/components/cart-tile/index.jsx
+++ src/components/cart-tile/index.jsx
@@ -11,12 +11,12 @@ const CartTile = ({ cartItem }) => {
 
   return (
     <>
-      <div className="flex items-center p-5 justify-between bg-red-500 mt-2 mb-2 rounded-xl">
-        <div className="flex p-3">
-          <img src={cartItem?.image} alt={cartItem?.title} className="h-28 rounded-lg" />
+      <div className="flex flex-col items-center p-5 justify-between bg-red-500 mt-2 mb-2 rounded-xl">
+        <div className="flex items-center flex-col p-3">
+          <img src={cartItem?.image} alt={cartItem?.title} className="h-28 w-28 rounded-lg" />
           <div className="ml-10 self-start space-y-5">
             <h1 className="text-xl text-white font-bold">{cartItem?.title}</h1>
-            <p className="text-white font-extrabold">{cartItem?.price}</p>
+            <p className="text-white font-extrabold">Price: {' '} {cartItem?.price}</p>
           </div>
         </div>
         <div>
